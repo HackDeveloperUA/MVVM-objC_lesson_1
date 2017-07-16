41c34b536007d8a18bbd00d9acf701367beaa0de
M1A1rx <thisismymail03@gmail.com>
2017-07-16 11:04:32 +0200

    Initial Commit

---
+++ MVVM-objC_lesson_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM-objC_lesson_1
+//
+//  Created by Uber on 16/07/2017.
+//  Copyright © 2017 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
